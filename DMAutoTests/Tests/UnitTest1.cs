c848b16460efec738cb46d2b8c808f419116d1a7
Akorium <komlevvanya72@gmail.com>
2024-09-09 00:18:39 +0700

    Demo test realisation

--- DMAutoTests/Tests/UnitTest1.cs
+++ DMAutoTests/Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using DMAutoTests;
 using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Input;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,22 @@ public class WinFormsAppTests : TestBase
     [Test]
     public void TestButtonClick()
     {
+        List<StaffMobile> staffMobiles = StaffMobile.GetAllData();
+        StaffMobile operatorToLogin = staffMobiles.First();
         Button primaryOperatorLoginButton = manager.MainWindow.FindFirstDescendant(cf => cf.ByAutomationId("_loginMainOperatorButton")).AsButton();
         primaryOperatorLoginButton.Click();
+        Keyboard.Type(operatorToLogin.Number);
         AutomationElement keyboard = manager.MainWindow.FindFirstDescendant(cf => cf.ByAutomationId("Keyboard"));
         AutomationElement[] keyboardButtons = keyboard.FindAllDescendants();
-        Button cancelButton = keyboardButtons.FirstOrDefault(btn => btn.Properties.Name.Value == "Отмена").AsButton();
-        cancelButton.Click();
+        Button submitButton = keyboardButtons.FirstOrDefault(btn => btn.Properties.Name.Value == "OK").AsButton();
+        submitButton.Click();
+        Label mainOperatorValueLabel = manager.MainWindow.FindFirstDescendant(cf => cf.ByAutomationId("_mainOperatorValueLabel")).AsLabel();
+        Assert.That($"{operatorToLogin.Name} {operatorToLogin.Surname}", Is.EqualTo(mainOperatorValueLabel.Text));
+    }
+    [TearDown]
+    public void Logout()
+    {
+        Button logoutButton = manager.MainWindow.FindFirstDescendant(cf => cf.ByAutomationId("_logoutMainOperatorButton")).AsButton();
+        logoutButton.Click();
     }
 }
